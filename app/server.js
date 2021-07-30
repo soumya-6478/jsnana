baf50aef4b14d3806734c831ef4d1d16abb27272
soumya-6478 <82015651+soumya-6478@users.noreply.github.com>
2021-07-30 18:26:19 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -21,7 +21,7 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally
-// added for testing
+// added for testing blabla
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 // use when starting application as docker container
